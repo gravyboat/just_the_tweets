510489af545eb138881793fa0fe1b7eb6ae994bd
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-03-26 05:43:38 +0000

    updated tweetScraper tp prep for passing in a list of users for sqlite implementation

--- tweetScraper.py
+++ tweetScraper.py
@@ -6,29 +6,39 @@ import argparse
 
 class tweetScraper(object):
 
-    def queryTwitterUserDatabase(self, twitterDatabase= 'twitterUsers.db')
-    def createJsonData(self, twitterHandle):
-        self.twitterHandle = twitterHandle
+    def queryTwitterUserDatabase(self, twitterDatabase = 'twitterUsers.db'):
+        pass
 
-        twitterUserUrl = 'https://api.twitter.com/1/users/show.json?screen_name={0}&include_entities=true'.format(self.twitterHandle)
-        jsonTweetData = urllib.request.urlopen(twitterUserUrl)
-        readTweetData = jsonTweetData.readall().decode('utf-8')
-        formattedJsonTweet = json.loads(readTweetData)
-        return(formattedJsonTweet)
+    def createJsonData(self, twitterHandles):
+        self.twitterHandles = twitterHandles
+        jsonList = []
 
-    def pullTweet(self, tweetJson):
-        self.tweetJson = tweetJson
+        for handle in self.twitterHandles:
+            twitterUserUrl = 'https://api.twitter.com/1/users/show.json?screen_name={0}&include_entities=true'.format(handle)
+            jsonTweetData = urllib.request.urlopen(twitterUserUrl)
+            readTweetData = jsonTweetData.readall().decode('utf-8')
+            formattedJsonTweet = json.loads(readTweetData)
+            jsonList.append(formattedJsonTweet)
 
-        return(self.tweetJson['screen_name'] + ': ' + self.tweetJson['status']['text'])
+        return(jsonList)
+
+    def pullTweet(self, jsonTweetList):
+        self.jsonTweetList = jsonTweetList
+        tweetList = []
+
+        for jsonTweet in self.jsonTweetList:
+            tweetList.append(jsonTweet['screen_name'] + ': ' + jsonTweet['status']['text'])
+        return(tweetList)
 
 
 parser = argparse.ArgumentParser(description='Choose how a user wishes to interact.')
 parser.add_argument('-u', '--user', dest='twitterUser', type=str, help='Choose which Twitter user you would like to query.')
 args = parser.parse_args()
 if args.twitterUser:
-    twitterHandle = args.twitterUser
+    twitterHandle = [args.twitterUser]
 
 if __name__ == '__main__':
     tweetScrape = tweetScraper()
+    handleId = ['gvanrossum', 'raymondh']
     twitterJson = tweetScrape.createJsonData(twitterHandle)
     print(tweetScrape.pullTweet(twitterJson))
